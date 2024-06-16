ac80bd622ebf3e33b6abd61bc674e476cce74e73
vierodan <vierodan@gmail.com>
2024-06-16 21:43:16 +0200

    extract_output.sh amended

--- extract_outputs.sh
+++ extract_outputs.sh
@@ -5,9 +5,9 @@ extract_value() {
     echo "$outputs" | jq -r ".[] | select(.OutputKey==\"$1\") | .OutputValue"
 }
 
-BASE_URL_API=$(extract_value "BaseUrlApi")
-DELETE_TODO_API=$(extract_value "DeleteTodoApi")
-LIST_TODOS_API=$(extract_value "ListTodosApi")
-UPDATE_TODO_API=$(extract_value "UpdateTodoApi")
-GET_TODO_API=$(extract_value "GetTodoApi")
-CREATE_TODO_API=$(extract_value "CreateTodoApi")
+export BASE_URL_API=$(extract_value "BaseUrlApi")
+export DELETE_TODO_API=$(extract_value "DeleteTodoApi")
+export LIST_TODOS_API=$(extract_value "ListTodosApi")
+export UPDATE_TODO_API=$(extract_value "UpdateTodoApi")
+export GET_TODO_API=$(extract_value "GetTodoApi")
+export CREATE_TODO_API=$(extract_value "CreateTodoApi")
