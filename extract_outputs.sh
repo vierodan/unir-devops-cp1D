2895df399cbcfb897d531af61034834f67479044
vierodan <vierodan@gmail.com>
2024-06-18 13:46:36 +0200

    pipeline amended

--- extract_outputs.sh
+++ extract_outputs.sh
@@ -19,15 +19,6 @@ extract_value() {
 }
 
 BASE_URL_API=$(extract_value "BaseUrlApi")
-#DELETE_TODO_API=$(extract_value "DeleteTodoApi")
-#LIST_TODOS_API=$(extract_value "ListTodosApi")
-#UPDATE_TODO_API=$(extract_value "UpdateTodoApi")
-#GET_TODO_API=$(extract_value "GetTodoApi")
-#CREATE_TODO_API=$(extract_value "CreateTodoApi")
 
 echo $BASE_URL_API > base_url_api.tmp
-#echo $DELETE_TODO_API > delete_todo_api.tmp
-#echo $LIST_TODOS_API > list_todos_api.tmp
-#echo $UPDATE_TODO_API > update_todo_api.tmp
-#echo $GET_TODO_API > get_todo_api.tmp
-#echo $CREATE_TODO_API >create_todo_api.tmp
+
