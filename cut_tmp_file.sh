e513dd66e429c1fc0e1139ca2a2c3f14f3e98df9
vierodan <vierodan@gmail.com>
2024-06-18 13:12:31 +0200

    pipeline amended

--- cut_tmp_file.sh
+++ cut_tmp_file.sh
@@ -11,6 +11,7 @@ file=$3
 
 echo "cut_tmp_file.sh --> Input 1 'content' value: $content"
 echo "cut_tmp_file.sh --> Input 2 'cut' value: $cut"
+echo "cut_tmp_file.sh --> Input 3 'file' value: $file"
 
 trimmed_content=$(echo "$content" | xargs)
 result=${trimmed_content:0:${#trimmed_content}-$cut}
