f97b3bd852615bef5b7d1a61c48bd7e5fec677ee
vierodan <vierodan@gmail.com>
2024-06-22 12:59:39 +0200

    get_base_url_api.sh amended

--- get_base_url_api.sh
+++ get_base_url_api.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
+#check script input parameters
 if [ "$#" -lt 2 ]; then
   echo "Usage: $0 <stage> <region>"
   exit 1
 fi
 
+#asign input parameters to localvariabbles
 stage=$1
 region=$2
 
+#check that Stage is not missing or empty
+if [ -z "$stage" ]; then
+    echo "Error: Stage is missing or empty."
+    exit 1
+fi
+
+#check that Region is not missing or empty
+if [[ -z "$region" ]; then
+    echo "Error: Region is missing or empty."
+    exit 1
+fi
+
+#echo of the input variables
 echo "get_base_url_api.sh --> Input 1 'stage' value: $stage"
 echo "get_base_url_api.sh --> Input 2 'region' value: $region"
 
+#execute aws describe stack command for getting the outputs of sam deploy command
+if [ "$stage" == "default" ]; then
+    outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws --region $region | jq '.Stacks[0].Outputs')
+        
+else    
+    outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws-$stage --region $region | jq '.Stacks[0].Outputs')
+fi
 
-outputs=$(aws cloudformation describe-stacks --stack-name todo-list-aws-$stage --region $region | jq '.Stacks[0].Outputs')
-
+# function for extracting using jq the base url value
 extract_value() {
-    echo "$outputs" | jq -r ".[] | select(.OutputKey==\"$1\") | .OutputValue"
+    echo "$1" | jq -r ".[] | select(.OutputKey==\"$2\") | .OutputValue"
 }
 
-BASE_URL_API=$(extract_value "BaseUrlApi")
+#extract base url value
+BASE_URL_API=$(extract_value "$outputs" "BaseUrlApi")
 
+#save base url value to fa temporal file
 echo $BASE_URL_API > base_url_api.tmp
+
 
